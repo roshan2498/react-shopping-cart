b1c7a5a73e7ab6c9863edd6cc90cf97ec5076666
roshan2498 <roshan_bt2k16@dtu.ac.in>
2022-01-31 18:29:06 +0530

    updated code

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,7 +4,7 @@ class Filter extends Component {
   render() {
     return (
       <div className="filter">
-        <div className="filter-result">{this.props.count}</div>
+        <div className="filter-result">{this.props.count} Products</div>
         <div className="filter-sort">
           Order{" "}
           <select value={this.props.sort} onChange={this.props.sortProducts}>
