797beb871edd2e30639f44e5268c75d6b291b974
roshan2498 <roshan_bt2k16@dtu.ac.in>
2022-02-01 13:39:13 +0530

    checkout form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // feature 1
 import React from "react";
+import Cart from "./components/Cart";
 import Filter from "./components/Filter";
 import Products from "./components/Products";
 import data from "./data.json";
@@ -11,6 +12,9 @@ class App extends React.Component {
       products: data.products,
       size: "",
       sort: "",
+      cartItems: localStorage.getItem("cartItems")
+        ? JSON.parse(localStorage.getItem("cartItems"))
+        : [],
     };
   }
 
@@ -50,6 +54,39 @@ class App extends React.Component {
       });
     }
   };
+
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    this.setState({
+      cartItems: cartItems.filter((x) => x._id !== product._id),
+    });
+    cartItems.filter((x) => x._id !== product._id);
+    localStorage.setItem(
+      "cartItems",
+      JSON.stringify(cartItems.filter((x) => x._id !== product._id))
+    );
+  };
+
+  createOrder = (order) => {
+    alert("Need to save order for" + order.name);
+  };
+
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
+    }
+
+    this.setState({ cartItems });
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  };
   render() {
     return (
       <div className="grid-container">
@@ -66,9 +103,18 @@ class App extends React.Component {
                 filterProducts={this.filterProducts}
                 sortProducts={this.sortProducts}
               />
-              <Products products={this.state.products} />
+              <Products
+                products={this.state.products}
+                addToCart={this.addToCart}
+              />
             </div>
-            <div className="sidebar">Cart Items</div>
+            <div className="sidebar">
+              <Cart
+                cartItems={this.state.cartItems}
+                removeFromCart={this.removeFromCart}
+                createOrder={this.createOrder}
+              />
+            </div>
           </div>
         </main>
         <footer>All rights is reserved.</footer>
